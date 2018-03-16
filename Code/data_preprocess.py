b4c48218cbcb612c47348131a39eb9b4559d7a6b
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-03-16 17:03:40 -0400

    Data process pipeline

--- Code/data_preprocess.py
+++ Code/data_preprocess.py
@@ -3,7 +3,6 @@ import numpy as np
 import shared_utils as su
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedShuffleSplit
-label_encoder=LabelEncoder()
 
 
 
@@ -17,9 +16,17 @@ def preprocess(filename):
     dataframe_all.columns = ["document_label", "word_values"]
     dataframe_all = dataframe_all.dropna(subset=['word_values'])
 
+    return dataframe_all
     # step 2 Make a stratified sample of the classes and split into test and train sets
 
-
+def create_train_test(dataframe_all):
+    """
+    Create train test split from the incoming data frame.
+    During training this will create both sets, during production we only need test
+    :param dataframe_all:
+    :return:
+    """
+    label_encoder=LabelEncoder()
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=42)
     for train_index, test_index in split.split(dataframe_all['word_values'], dataframe_all['document_label']):
         strat_train_set = dataframe_all.loc[train_index]
@@ -27,12 +34,22 @@ def preprocess(filename):
 
     strat_train_set = strat_train_set.dropna(subset=['word_values'])
     strat_test_set = strat_test_set.dropna(subset=['word_values'])
-
-    x_train, y_train = su.pipe(strat_train_set), label_encoder.fit_transform(
+    pipe=su.pipe()
+    x_train, y_train = pipe.fit_transform(strat_train_set), label_encoder.fit_transform(
         strat_train_set['document_label'])
-    x_test, y_test = su.pipe(strat_test_set), label_encoder.fit_transform(
+    x_test, y_test = pipe.transform(strat_test_set), label_encoder.fit_transform(
         strat_test_set['document_label'])
 
     return x_train,x_test,y_train,y_test
 
+def prod_test(dataframe):
+    """
+    Given the documents for test time, we need to predict the correct class.
+    So the features need to be processed to predict wih the loaded model
+    :param dataframe:
+    :return:
+    """
+    dataframe.columns = ["word_values"]
+    dataframe = dataframe.dropna(subset=['word_values'])
+    return  su.pipe().transform(dataframe)
 
